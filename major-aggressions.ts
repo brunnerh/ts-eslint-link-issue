a60428a13fcab866b99bc29c1c11391b00f35209
brunnerh <brunnerh@users.noreply.github.com>
2023-11-17 09:18:45 +0100

    Init.

---
+++ major-aggressions.ts
@@ -0,0 +1,9 @@
+import { throwStones } from './minor-aggressions';
+
+/**
+ * Cause serious international side effects.  
+ * For less serious side effects, use {@link throwStones} instead.
+ */
+export function fireMissiles() {
+	// ...
+}
---
+++ minor-aggressions.ts
@@ -0,0 +1,7 @@
+/**
+ * Cause less serious side effects.  
+ * (Unless aimed at an important official.)
+ */
+export function throwStones() {
+	// ...
+}
